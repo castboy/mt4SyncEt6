210014d45233da6123e8ee1f8665220295604d09
tutengdihuang@163.com <tutengdihuang@163.com>
2020-01-03 18:58:33 +0800

    update

--- session_test.go
+++ session_test.go
@@ -1,6 +1,7 @@
 package mt4SyncEt6
 
 import (
+	"encoding/json"
 	"fmt"
 	"mt4SyncEt6/session"
 	"testing"
@@ -17,3 +18,42 @@ func TestMt4ToEt6(t *testing.T) {
 	et6 := session.Mt4ToEt6(mt4)
 	fmt.Println(et6)
 }
+
+func TestConvet(t *testing.T)  {
+
+	//prepare format and space for json convert
+	var infoSlice=make([]SessionInfo,300)
+	for k,_:=range infoSlice{
+		if k<300{
+			infoSlice[k].Trade_session= make(map[string]string,7)
+		}
+
+	}
+	//json convert
+	err:=json.Unmarshal([]byte(SesssionStr),&infoSlice)
+	if err!=nil{
+		t.Fatalf("json.Unmarshal error:%+v\n",err)
+	}
+
+	//change mt4 to et6
+	et6Sessions:=make([]Et6Session,0)
+	for _,v:=range infoSlice{
+		if v.Symbol_name==""{
+			t.Log("Empty symbol info")
+			break
+		}
+		trade_session:=session.Mt4ToEt6(v.Trade_session)
+		et6Session:=Et6Session{
+			Symbol_name:v.Symbol_name,
+			Trade_session:trade_session,
+		}
+		et6Sessions = append(et6Sessions, et6Session)
+	}
+	t.Log(et6Sessions)
+	// save to db
+	for _,v:=range et6Sessions{
+		SessionToDB(v)
+	}
+}
+
+
---
+++ tools.go
@@ -0,0 +1,161 @@
+package mt4SyncEt6
+
+import (
+	"fmt"
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/go-xorm/xorm"
+	"mt4SyncEt6/decimal"
+	"strconv"
+	"time"
+)
+
+func SessionToDB(v Et6Session){
+	enginEt6,err:=NewET6EngineXorm()
+	if err!=nil{
+		return
+	}
+	sourceItem:=new(Source)
+	//_, err = enginEt6.Table("source").Where("source=?", v.Symbol_name).NoAutoCondition(true).Get(sourceItem)
+	hit, err := enginEt6.Table("source").Where("source=?", v.Symbol_name).NoAutoCondition(true).Get(sourceItem)
+	if !hit && err == nil {
+		return
+	}
+	//insert session to db
+	// map
+	for k,tradeSession:=range v.Trade_session{
+			//slice
+			for _,span:=range tradeSession{
+				sess:=Session{}
+				sess.Type=0
+				sess.SourceID=sourceItem.ID
+				weekDay,_:= strconv.Atoi(k)
+				if weekDay==0{
+					sess.Weekday=time.Sunday
+				}else if weekDay==1 {
+					sess.Weekday=time.Monday
+				}else if weekDay==2 {
+					sess.Weekday=time.Tuesday
+				}else if weekDay==3 {
+					sess.Weekday=time.Wednesday
+				}else if weekDay==4 {
+					sess.Weekday=time.Thursday
+				}else if weekDay==5 {
+					sess.Weekday=time.Friday
+				}else if weekDay==6 {
+					sess.Weekday=time.Saturday
+				}
+				sess.TimeSpan=span
+
+				//trade type
+				sess.Type=0
+				_, err = enginEt6.Table("session").Omit("id").InsertOne(sess)
+				sess.Type=1
+				_, err = enginEt6.Table("session").Omit("id").InsertOne(sess)
+				if err!=nil{
+					fmt.Println("err")
+				}
+			}
+		}
+}
+
+func NewET6EngineXorm()(*xorm.Engine,error){
+	dataSourceName := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8",
+		"glmt4dev_wr", "mt4geed0Uokohphai1UNgeep5ae", "devcondb.r62g.cn",
+		"3306","trading_system")
+
+	mt4Engine, err := xorm.NewEngine("mysql", dataSourceName)
+	if err != nil {
+		return nil,err
+	}
+	mt4Engine.SetMaxOpenConns(100)
+	mt4Engine.SetMaxIdleConns(20)
+	mt4Engine.SetConnMaxLifetime(1800 * time.Second)
+	//engine.DatabaseTZ = time.UTC
+	//engine.TZLocation = time.UTC
+
+	return mt4Engine, nil
+}
+
+
+type SessionInfo struct {
+	Symbol_name 	string				`json:"symbol_name"`
+	Trade_session   map[string]string   `json:"trade_session"`
+}
+
+type Et6Session struct {
+	Symbol_name 	string				`json:"symbol_name"`
+	Trade_session   map[string][]string   `json:"trade_session"`
+}
+
+type Source struct {
+	ID             int             `json:"id" xorm:"id autoincr"`
+	Source         string          `json:"source" xorm:"source"`
+	SourceCN       string          `json:"source_cn" xorm:"source_cn"`
+	SourceType     SourceType      `json:"source_type" xorm:"source_type"`
+	Digits         int             `json:"digits" xorm:"digits"`
+	Multiply       decimal.Decimal `json:"multiply" xorm:"multiply"`
+	ContractSize   decimal.Decimal `json:"contract_size" xorm:"contract_size"`
+	StopsLevel     int             `json:"stops_level" xorm:"stops_level"`
+	ProfitMode     ProfitMode      `json:"profit_mode" xorm:"profit_mode"`
+	ProfitCurrency string          `json:"profit_currency" xorm:"profit_currency"`
+	MarginMode     MarginMode      `json:"margin_mode" xorm:"margin_mode"`
+	MarginCurrency string          `json:"margin_currency" xorm:"margin_currency"`
+	SwapType       SwapType        `json:"swap_type" xorm:"swap_type"`
+	SwapLong       decimal.Decimal `json:"swap_long" xorm:"swap_long"`
+	SwapShort      decimal.Decimal `json:"swap_short" xorm:"swap_short"`
+	SwapCurrency   string          `josn:"swap_currency" xorm:"swap_currency"`
+	Swap3Day       time.Weekday    `json:"swap_3_day" xorm:"swap_3_day"`
+}
+
+type Session struct {
+	ID       int          `xorm:"id autoincr"`
+	SourceID int          `xorm:"source_id"`
+	Type     SessionType  `xorm:"type"`
+	Weekday  time.Weekday `xorm:"weekday"`
+	TimeSpan string       `xorm:"time_span"`
+}
+
+type SessionType int
+
+const (
+	Quote SessionType = iota
+	Trade
+)
+type (
+	ProfitMode int
+	SwapType   int
+	MarginMode int
+	SourceType int
+)
+
+const (
+	MarginForex MarginMode = iota
+	MarginCfd
+	MarginFutures
+	MarginCfdIndex
+	MarginCfdLeverage
+)
+
+const (
+	ByPoints SwapType = iota
+	ByMoney
+	ByInterest
+	ByMoneyInMarginCurrency
+	ByInterestOfCfds
+	ByInterestOfFutures
+)
+
+
+const (
+	SourceFx SourceType = iota
+	SourceMetal
+	SourceEnergy
+	SourceIndex
+	SourceCrypto
+)
+
+const (
+	ProfitForex ProfitMode = iota
+	ProfitCfd
+	ProfitFutures
+)
